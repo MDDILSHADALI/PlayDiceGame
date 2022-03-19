08ca833f7975c36e8ad7633a6841843714df5ba3
99dillu1 <93716926+MDDILSHADALI@users.noreply.github.com>
2022-03-19 20:10:39 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+var randomNumber1 = Math.floor(Math.random() * 6) +1;
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+var randomImageSource = "images/" + randomDiceImage;
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomImageSource);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+
+
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML="🏳️‍🌈Player 1Wins!";
+}
+else if(randomNumber2 > randomNumber1){
+    document.querySelector("h1").innerHTML="🏳️‍🌈Player 2Wins!";
+}
+else{
+    document.querySelector("h1").innerHTML="Draw!";
+}
